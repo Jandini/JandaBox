0310bf6eee0c1363d59ecc48dd0eecdbc1ee450e
Matt <matt.janda.kingston@gmail.com>
2023-03-26 08:57:56 +0000

    Add appsettings to health endpoint

--- src/WebApi/src/WebApiBox/Services/Health/HealthService.cs
+++ src/WebApi/src/WebApiBox/Services/Health/HealthService.cs
@@ -4,20 +4,36 @@ namespace WebApiBox.Services
 {
     public class HealthService : IHealthService
     {
+#if (appSettings)
+        private readonly AppSettings _settings;
+
+        public HealthService(AppSettings settings)
+        {
+            _settings = settings;
+        }
+
+#endif
         public async Task<HealthInfo> GetHealthInfoAsync()
         {
-            var assembly = Assembly.GetExecutingAssembly();
-            var version = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
-
+#if (appSettings)
             var info = new HealthInfo
             {
                 Service = new WebApiHealthInfo()
                 {
-                    Name = assembly.GetName().Name,
-                    Version = version,
+                    Name = _settings.ApplicationName ?? Assembly.GetExecutingAssembly().GetName().Name,
+                    Version = _settings.ApplicationVersion ?? Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
                 }
             };
-
+#else
+            var info = new HealthInfo
+            {
+                Service = new WebApiHealthInfo()
+                {
+                    Name = Assembly.GetExecutingAssembly().GetName().Name,
+                    Version = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+                }
+            };
+#endif
             return await Task.FromResult(info);
         }
     }
