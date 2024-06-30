e1d43704b104ab1911a68cd55ea3ec36de30ac08
Matt Janda <matt.janda.kingston@gmail.com>
2024-06-30 19:27:22 +0100

    Add Service box (#37)
    * Add service files
    
    * Add service box template
    
    * Update readme file

---
+++ src/Service/ServiceBoxService.cs
@@ -0,0 +1,20 @@
+﻿#if (logger)
+using Microsoft.Extensions.Logging;
+
+#endif
+namespace NAMESPACE_NAME
+{
+    internal class ServiceBoxService : IServiceBoxService
+    {
+#if (logger)
+        private readonly ILogger<ServiceBoxService> _logger;
+
+        public ServiceBoxService(ILogger<ServiceBoxService> logger) 
+        {
+            _logger = logger;
+        }
+#else
+
+#endif
+    }
+}
---
+++ src/Service/ServiceBoxServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace NAMESPACE_NAME
+{
+    public static class ServiceBoxServiceExtensions
+    {
+        public static IServiceCollection AddServiceBoxService(this IServiceCollection services)
+        {
+            return services.AddTransient<IServiceBoxService, ServiceBoxService>();
+        }
+    }
+}
