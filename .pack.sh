eb303e8de9426b19f21a6fb4c6e1ad40c82162d1
Matt Janda <matt.janda.kingston@gmail.com>
2025-01-17 00:37:48 +0000

    Linux vscode config scripts (#61)
    * Remove version from program files in the templates
    
    * Automatically update template version in program file comments
    
    * Use single line run for Nuget command
    
    * Run build on ubuntu 20.04
    
    * Add version comment preview and use nuget available in ubuntu 20.04
    
    * Optimize comment update step in actions pipeline
    
    * Cleanup program file
    
    * Do not use dollar sign when assigning variables in bash
    
    * Consolidate powershell scripts
    
    * Add linux version of pack
    
    * Automatically install mono and nuget if not exist
    
    * Add launch for installing and uninstalling the templates locally
    
    * Include vscode folder
    
    * Add tasks json file
    
    * Add version update in pack script

---
+++ .pack.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check and install Mono if not present
+if ! command -v mono &> /dev/null; then
+  echo "Mono is not installed. Installing..."
+  sudo apt update
+  sudo apt install -y mono-devel
+else
+  echo "Mono is already installed."
+fi
+
+# Check and install NuGet if not present
+if [ ! -f /usr/local/bin/nuget.exe ]; then
+  echo "NuGet is not installed. Installing..."
+  sudo wget -O /usr/local/bin/nuget.exe https://dist.nuget.org/win-x86-commandline/latest/nuget.exe
+else
+  echo "NuGet is already installed."
+fi
+
+# Get GitVersion
+GIT_VERSION=$(dotnet gitversion /showvariable SemVer)
+HEADER="Created with JandaBox"
+REPLACEMENT="$HEADER $GIT_VERSION"
+
+echo Updating version in Program.cs files.
+find . -type f -name "Program.cs" -exec sed -i "s/$HEADER/$REPLACEMENT/g" {} +
+
+# Pack the NuGet package using Mono
+mono /usr/local/bin/nuget.exe pack .nuspec -OutputDirectory bin/Release -NoDefaultExcludes -Version $GIT_VERSION
+
+echo Reverting version change in Program.cs files.
+find . -type f -name "Program.cs" -exec sed -i "s/$REPLACEMENT/$HEADER/g" {} +
+
+# Uninstall and reinstall the NuGet package
+dotnet new uninstall JandaBox > /dev/null
+dotnet new install bin/Release/JandaBox.$GIT_VERSION.nupkg
