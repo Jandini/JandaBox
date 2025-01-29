db96aeedfcd92b57939c37b730caa50d2c364f88
Matt Janda <matt.janda.kingston@gmail.com>
2025-01-29 00:06:07 +0000

    Resolve #45 Add service start and up time to health endpoint (#91)

--- src/WebApi/src/WebApiBox/Services/Health/ServiceInfo.cs
+++ src/WebApi/src/WebApiBox/Services/Health/ServiceInfo.cs
@@ -4,4 +4,6 @@ public class ServiceInfo
 {
     public string? Name { get; set; }
     public string? Version { get; set; }
+    public DateTime StartedOn { get; set; }
+    public TimeSpan UpTime { get; set; }
 }
